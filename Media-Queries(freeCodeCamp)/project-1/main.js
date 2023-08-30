62204199c2626bb506575775d1a4eef238c2b01a
Harshal Sonawane <hsonawane154@gmail.com>
2023-08-30 11:41:35 +0530

    successfully completed 3 prjects which is related to respinsive design using media queries.

---
+++ Media-Queries(freeCodeCamp)/project-1/main.js
@@ -0,0 +1,9 @@
+window.onresize = screen;
+
+window.onload = screen;
+
+function screen() {
+  const myWidth = window.innerWidth;
+
+  document.getElementById("size").innerHTML = "Width : " + myWidth + "px";
+}
